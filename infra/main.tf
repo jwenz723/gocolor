7f7887a5b41955896d14dfd186a9ac0e71811b94
Jeff Wenzbauer <jeff.wenzbauer@niceincontact.com>
2020-11-09 11:46:56 -0700

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,17 +1,2 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.8.0"
-    }
-  }
-}
-
-resource "aws_s3_bucket" "test" {
-  bucket = "jeff-${var.bucket_suffix}"
-}
-
-variable "bucket_suffix" {
-  default = "test"
-  type = string
+resource "null_resource" "example" {
 }
